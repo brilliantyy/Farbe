fbffe0295edf37cea5a300014fb35cda4fe932fd
brilliantyy <yujia6366@163.com>
2020-07-14 23:53:00 +0800

    first commit

---
+++ bin/init.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const commander = require('commander')
+const path = require('path')
+const glob = require('glob')
+const fs = require('fs')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
